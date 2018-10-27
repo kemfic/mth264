e9e5a370836db2319df67695891a6b88e910fa15
kemfic1 <19ficici.kemal@gmail.com>
2018-10-27 09:42:50 -0400

    minor changes

--- project2/project2.m
+++ project2/project2.m
@@ -27,12 +27,14 @@ f1 = @(x) (x / sin(x));
 f2 = @(x) ((exp(x)-1)/sin(x)); 
 f3 = @(x) (asin(x)/x);
 
-ddf1 = @(x) (csc(x)*(-2*cot(x) + x*cot^2(x) + x*csc^2(x)));
+
+ddf1 = @(x) (csc(x)*((0-2)*cot(x) + x*cot(x)^2 + x*csc(x)^2));
 ddf2 = @(x) (csc(x)*(exp(x) - 2*exp(x)*cot(x) + ((exp(x) - 1)*(cot(x)^2 + csc(x)^2))));
 ddf3 = @(x) ((2*asin(x))/x^3 - 2/(x^2 * sqrt(1-x^2)) + 1/sqrt(( 1-x^2)^3));
 
 f = f1;
 ddf = ddf1;
+method = simpson % simpson, exSimp, compositeMid
 
 a = 0.0001;
 b = 0.999;
@@ -41,10 +43,14 @@ n = 6;
 error = 100;
 
 hCur = 0;
-hPrev = 0;
+hPrev = -50;
 
-while abs(error) > 0.0001:
+while abs(error) > 0.0001
+    dx = (b-a) / n;
+    x = a:dx:b;
     
+    hCur = method();
     
-    
+    error = hCur - hPrev;
+    hPrev = hCur;
 end
