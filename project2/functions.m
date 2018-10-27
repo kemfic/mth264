c4cc5500df6499980e60b3c455852b866d83ac84
kemfic1 <19ficici.kemal@gmail.com>
2018-10-27 08:42:16 -0400

    minor cleanup

--- project2/functions.m
+++ project2/functions.m
@@ -6,8 +6,6 @@ function out = simpson(a, b, dx):
     x1 = x(1:2:end); %Every even Interval
     x2 = x(2:2:end); %Every odd Interval
     out = (((4*sum(f(x1))) + (2*sum(f(x2))) + f(a) + f(b))* (dx / 3));
-    
-end
 
 function out = compositeMid(a, b, dx):
     x = a+dx :dx: b-dx;
@@ -15,14 +13,12 @@ function out = compositeMid(a, b, dx):
     g = cat(2, [0;], x);
     g1  =cat(2, x, [0;]);
     
-    g = (g+g1) / 2
-    g = g[2:end-1]
+    g = (g+g1) / 2;
+    g = g[2:end-1];
     
     out = ((b - a)* h.**2 / 24);
     out = out + df( (a+b)/2 );
-    out = dx * sum( f(g) )
-    
-end
+    out = dx * sum( f(g) );
 
 function out = exSimpson(a, b, dx):
     x = a+dx :dx: b-dx;
@@ -31,4 +27,3 @@ function out = exSimpson(a, b, dx):
     out = out + 48 * sum(f(x[5: end-4]));
     out = out + 49*f(x[end-3]) + 43*f(x[end-2]) + 59*f(x[end-1]) + 17*f(x[end]);            
     out = (dx/48) * out;
-end
