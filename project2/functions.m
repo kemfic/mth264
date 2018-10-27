5046d42b97152ad6961a4f2f3509f108581ae6cc
kemfic1 <19ficici.kemal@gmail.com>
2018-10-27 08:41:28 -0400

    added compositemidpoint

--- project2/functions.m
+++ project2/functions.m
@@ -11,19 +11,24 @@ end
 
 function out = compositeMid(a, b, dx):
     x = a+dx :dx: b-dx;
-    x1 = x(1:2:end); %Every even Interval
-    x2 = x(2:2:end); %Every odd Interval
-    out = (((4*sum(f(x1))) + (2*sum(f(x2))) + f(a) + f(b))* (dx / 3));
+    
+    g = cat(2, [0;], x);
+    g1  =cat(2, x, [0;]);
+    
+    g = (g+g1) / 2
+    g = g[2:end-1]
+    
+    out = ((b - a)* h.**2 / 24);
+    out = out + df( (a+b)/2 );
+    out = dx * sum( f(g) )
     
 end
 
 function out = exSimpson(a, b, dx):
     x = a+dx :dx: b-dx;
-    x1 = x(1:2:end); %Every even Interval
-    x2 = x(2:2:end); %Every odd Interval
     
-    out = 17*f(x[1]) + 59*f(x[2]) + 43*f(x[3]) + 49*f(x[4]) 
-    out = out + 48 * sum(f(x[5: end-4])) 
+    out = 17*f(x[1]) + 59*f(x[2]) + 43*f(x[3]) + 49*f(x[4]);
+    out = out + 48 * sum(f(x[5: end-4]));
     out = out + 49*f(x[end-3]) + 43*f(x[end-2]) + 59*f(x[end-1]) + 17*f(x[end]);            
     out = (dx/48) * out;
 end
