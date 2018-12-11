ce0084aa9f9e047c9e2770d9b4108acaa6093e73
kemfic <19ficici.kemal@gmail.com>
2018-12-11 09:28:46 -0500

    added LaTeX files for report

---
+++ project3/code/p3_b.m
@@ -0,0 +1,33 @@
+% Problem 2
+% Monte Carlo Integration with Trig Distribution
+clear
+clc
+format long
+
+% Assigned Functions
+f1 = @(x) sin(x.^2);
+f2 = @(x) (x./sin(x)); 
+f3 = @(x) (exp(x)-1)/sin(x);
+
+
+f = f1;
+a=0;
+b=pi/2;
+
+n = 1000;
+dx = (b-a)/n;
+
+X = a:dx:b;
+fx = f(X);
+M = max((fx))+dx;
+
+intervals = transpose((1:n)) *(pi/n);
+I = sin(intervals);
+rando = rand(n,1);
+
+x = a + I*(b-a);
+
+y = rando * M;
+mask = y < f(x);
+
+MCL_Integral = mean(mask)*(M*(b-a))
